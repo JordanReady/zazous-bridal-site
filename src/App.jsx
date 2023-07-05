8ef8116144ff451158e678bb7ad9fdc57b5ce804
JordanReady <jready112@gmail.com>
2023-07-04 20:05:24 -0500

    add: navbar functionallity

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
+import "bootstrap/dist/js/bootstrap.bundle";
+
+import { Navbar, Footer } from "./components";
 
 import {
   Home,
@@ -20,43 +23,48 @@ import {
   WriteReview,
   Events,
 } from "./pages";
-import { Navbar, Footer } from "./components";
 
 const App = () => (
   <Router>
-    <Navbar />
-    <Routes>
-      <Route path="/" exact element={<Home />} />
-      <Route path="/our-story" exact element={<OurStory />} />
-      <Route path="/vendors" exact element={<Vendors />} />
-      <Route path="/bridal" exact element={<Bridal />} />
-      <Route path="/bridesmaids-mothers" exact element={<Bridesmaids />} />
-      <Route path="/prom" exact element={<Prom />} />
-      <Route path="/flowergirl-firstcommunion" exact element={<FlowerGirl />} />
-      <Route path="/accessories" exact element={<Accessories />} />
-      <Route path="/suits-tuxedos" exact element={<SuitsTuxedos />} />
-      <Route
-        path="/how-to-measure-yourself"
-        exact
-        element={<HowToMeasureYourself />}
-      />
-      <Route
-        path="/schedule-appointment"
-        exact
-        element={<ScheduleAppointment />}
-      />
-      <Route path="/plan-appointment" exact element={<PlanAppointment />} />
-      <Route
-        path="/private-appointment"
-        exact
-        element={<PrivateAppointment />}
-      />
-      <Route path="/rave-reviews" exact element={<RaveReviews />} />
-      <Route path="/write-review" exact element={<WriteReview />} />
-      <Route path="/events" exact element={<Events />} />
-      <Route path="/contact" exact element={<Contact />} />
-    </Routes>
-    <Footer />
+    <div className="App">
+      <Navbar />
+      <Routes>
+        <Route path="/" exact element={<Home />} />
+        <Route path="/our-story" exact element={<OurStory />} />
+        <Route path="/vendors" exact element={<Vendors />} />
+        <Route path="/bridal" exact element={<Bridal />} />
+        <Route path="/bridesmaids-mothers" exact element={<Bridesmaids />} />
+        <Route path="/prom" exact element={<Prom />} />
+        <Route
+          path="/flowergirl-firstcommunion"
+          exact
+          element={<FlowerGirl />}
+        />
+        <Route path="/accessories" exact element={<Accessories />} />
+        <Route path="/suits-tuxedos" exact element={<SuitsTuxedos />} />
+        <Route
+          path="/how-to-measure-yourself"
+          exact
+          element={<HowToMeasureYourself />}
+        />
+        <Route
+          path="/schedule-appointment"
+          exact
+          element={<ScheduleAppointment />}
+        />
+        <Route path="/plan-appointment" exact element={<PlanAppointment />} />
+        <Route
+          path="/private-appointment"
+          exact
+          element={<PrivateAppointment />}
+        />
+        <Route path="/rave-reviews" exact element={<RaveReviews />} />
+        <Route path="/write-review" exact element={<WriteReview />} />
+        <Route path="/events" exact element={<Events />} />
+        <Route path="/contact" exact element={<Contact />} />
+      </Routes>
+      {/* <Footer /> */}
+    </div>
   </Router>
 );
 
