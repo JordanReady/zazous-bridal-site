2bcf4e41f7c9255d73adf72fad2af7c085ae93dd
JordanReady <jready112@gmail.com>
2023-07-06 17:46:37 -0500

    :fix carousel, hero text/button

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,9 +2,10 @@ import React, { useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Carousel } from "bootstrap";
 import Img1 from "../assets/property-pic-1.jpg";
-import Img2 from "../assets/property-pic-2.jpeg";
+import Img2 from "../assets/property-pic-2.jpg";
 import Img3 from "../assets/property-pic-3.jpg";
 import Img4 from "../assets/property-pic-4.jpg";
+import Img5 from "../assets/property-pic-5.jpg";
 
 import "../styles/Home.css";
 
@@ -15,6 +16,11 @@ const Home = () => {
     new Carousel(carouselElement);
   }, []);
 
+  const handleClick = (e) => {
+    e.preventDefault();
+    window.location.href = "/schedule-appointment";
+  };
+
   return (
     <div className="container">
       <div className="carousel slide carousel-fade" data-bs-ride="carousel">
@@ -23,22 +29,39 @@ const Home = () => {
             data-bs-target=".carousel"
             data-bs-slide-to="0"
             className="active"
+            style={{ backgroundImage: `url(${Img1})` }}
           />
-          <li data-bs-target=".carousel" data-bs-slide-to="1" />
-          <li data-bs-target=".carousel" data-bs-slide-to="2" />
-          <li data-bs-target=".carousel" data-bs-slide-to="3" />
+          <li
+            data-bs-target=".carousel"
+            data-bs-slide-to="1"
+            style={{ backgroundImage: `url(${Img2})` }}
+          />
+          <li
+            data-bs-target=".carousel"
+            data-bs-slide-to="2"
+            style={{ backgroundImage: `url(${Img3})` }}
+          />
+          <li
+            data-bs-target=".carousel"
+            data-bs-slide-to="3"
+            style={{ backgroundImage: `url(${Img4})` }}
+          />
+          <li
+            data-bs-target=".carousel"
+            data-bs-slide-to="4"
+            style={{ backgroundImage: `url(${Img5})` }}
+          />
         </ol>
 
         <div className="carousel-inner">
           <div className="carousel-item active ratio-4x3">
             <img
-              src={Img3}
+              src={Img1}
               className="d-block w-100 carousel-image"
               alt="Slide 1"
             />
             <div className="carousel-caption">
-              <h3>Slide 1 Title</h3>
-              <p>Slide 1 Description</p>
+              <h3 className="caption-text">Slide 1 Title</h3>
             </div>
           </div>
           <div className="carousel-item ratio-4x3">
@@ -48,8 +71,7 @@ const Home = () => {
               alt="Slide 2"
             />
             <div className="carousel-caption">
-              <h3>Slide 2 Title</h3>
-              <p>Slide 2 Description</p>
+              <h3 className="caption-text">Slide 2 Title</h3>
             </div>
           </div>
           <div className="carousel-item ratio-4x3">
@@ -59,19 +81,27 @@ const Home = () => {
               alt="Slide 3"
             />
             <div className="carousel-caption">
-              <h3>Slide 3 Title</h3>
-              <p>Slide 3 Description</p>
+              <h3 className="caption-text">Slide 3 Title</h3>
             </div>
           </div>
           <div className="carousel-item ratio-4x3">
             <img
-              src={Img2}
+              src={Img4}
               className="d-block w-100 carousel-image"
               alt="Slide 4"
             />
             <div className="carousel-caption">
-              <h3>Slide 4 Title</h3>
-              <p>Slide 4 Description</p>
+              <h3 className="caption-text">Slide 4 Title</h3>
+            </div>
+          </div>
+          <div className="carousel-item ratio-4x3">
+            <img
+              src={Img5}
+              className="d-block w-100 carousel-image"
+              alt="Slide 5"
+            />
+            <div className="carousel-caption">
+              <h3 className="caption-text">Slide 5 Title</h3>
             </div>
           </div>
         </div>
@@ -95,6 +125,7 @@ const Home = () => {
           <span className="visually-hidden">Next</span>
         </a>
       </div>
+      <div className="thumbnail-spacer"></div>
       <section className="hero-text-container">
         <h2 className="hero-text">
           Zazou’s Bridal Boutique & Tuxedos prides ourselves on being innovative
@@ -104,7 +135,9 @@ const Home = () => {
           in the Dubuque Millwork District is the perfect environment to help
           any bride say, "I found the gown at Zazou's!”
         </h2>
-        <button className="hero-button">Schedule Appointment</button>
+        <button className="hero-button btn btn-dark" onClick={handleClick}>
+          Schedule Appointment
+        </button>
       </section>
     </div>
   );
