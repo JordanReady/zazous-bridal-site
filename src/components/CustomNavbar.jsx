6f73d46718b20dcc80543f87e016063581d8e519
JordanReady <jready112@gmail.com>
2023-07-26 12:47:41 -0500

    fix: simple thicc nav logo, placement adjustments

--- src/components/CustomNavbar.jsx
+++ src/components/CustomNavbar.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Navbar, Nav, NavDropdown } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Logo from "../assets/Logos/Logo-outline-thicc.png";
+import Logo from "../assets/Logos/Logo-Nav.png";
 
 import "../styles/CustomNavbar.css";
 
