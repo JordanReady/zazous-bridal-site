6570a6533a9808f7a0d7b283b93531c4321902a3
JordanReady <jready112@gmail.com>
2023-07-28 12:37:05 -0500

    fix: change navbar look

--- src/components/CustomNavbar.jsx
+++ src/components/CustomNavbar.jsx
@@ -3,7 +3,9 @@ import { Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Navbar, Nav, NavDropdown } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Logo from "../assets/Logos/Logo-Nav.png";
+// import Logo from "../assets/Logos/Logo-Nav.png";
+import Logo from "../assets/Logos/Logo-Nav-Skinny-No-Outline.png";
+// import Logo from "../assets/Logos/Logo-Nav-No-Outline.png";
 
 import "../styles/CustomNavbar.css";
 
