a31ef7f1419d8c61f6a6a6fc62d53ad3dbc7522b
JordanReady <jready112@gmail.com>
2023-08-14 15:23:30 -0500

    fix: navbar active links

--- src/components/CustomNavbar.jsx
+++ src/components/CustomNavbar.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useRef } from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Navbar, Nav, NavDropdown } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -11,6 +11,7 @@ const CustomNavbar = () => {
   const [expanded, setExpanded] = useState(false);
   const [activeDropdown, setActiveDropdown] = useState(null);
   const dropdownTimerRef = useRef(null);
+  const location = useLocation();
 
   const handleNavItemClick = () => {
     setExpanded(false);
@@ -57,21 +58,34 @@ const CustomNavbar = () => {
         }`}
       >
         <Nav className="ml-auto nav-links">
-          <Nav.Link as={Link} to="/" onClick={handleNavItemClick}>
+          <Nav.Link
+            as={Link}
+            to="/"
+            onClick={handleNavItemClick}
+            className={location.pathname === "/" ? "active-link" : ""}
+          >
             Home
           </Nav.Link>
           <NavDropdown
             title="About Us"
-            className="dropdown"
             id="aboutDropdown"
             onMouseEnter={() => handleDropdownEnter("aboutDropdown")}
             onMouseLeave={() => handleDropdownLeave("aboutDropdown")}
             show={activeDropdown === "aboutDropdown"}
+            className={
+              location.pathname.includes("/our-story") ||
+              location.pathname.includes("/vendors")
+                ? "active-dropdown dropdown"
+                : "dropdown"
+            }
           >
             <NavDropdown.Item
               as={Link}
               to="/our-story"
               onClick={handleNavItemClick}
+              className={
+                location.pathname === "/our-story" ? "active-link" : ""
+              }
             >
               Our Story
             </NavDropdown.Item>
@@ -79,22 +93,34 @@ const CustomNavbar = () => {
               as={Link}
               to="/vendors"
               onClick={handleNavItemClick}
+              className={location.pathname === "/vendors" ? "active-link" : ""}
             >
               Vendors
             </NavDropdown.Item>
           </NavDropdown>
           <NavDropdown
             title="Dresses"
-            className="dropdown"
             id="dressesDropdown"
             onMouseEnter={() => handleDropdownEnter("dressesDropdown")}
             onMouseLeave={() => handleDropdownLeave("dressesDropdown")}
             show={activeDropdown === "dressesDropdown"}
+            className={
+              location.pathname.includes("/bridal") ||
+              location.pathname.includes("/bridesmaids-mothers") ||
+              location.pathname.includes("/prom") ||
+              location.pathname.includes("/flowergirl-firstcommunion") ||
+              location.pathname.includes("/accessories") ||
+              location.pathname.includes("/suits-tuxedos") ||
+              location.pathname.includes("/how-to-measure-yourself")
+                ? "active-dropdown dropdown"
+                : "dropdown"
+            }
           >
             <NavDropdown.Item
               as={Link}
               to="/bridal"
               onClick={handleNavItemClick}
+              className={location.pathname === "/bridal" ? "active-link" : ""}
             >
               Bridal
             </NavDropdown.Item>
@@ -102,16 +128,31 @@ const CustomNavbar = () => {
               as={Link}
               to="/bridesmaids-mothers"
               onClick={handleNavItemClick}
+              className={
+                location.pathname === "/bridesmaids-mothers"
+                  ? "active-link"
+                  : ""
+              }
             >
               Bridesmaids & Mothers
             </NavDropdown.Item>
-            <NavDropdown.Item as={Link} to="/prom" onClick={handleNavItemClick}>
+            <NavDropdown.Item
+              as={Link}
+              to="/prom"
+              onClick={handleNavItemClick}
+              className={location.pathname === "/prom" ? "active-link" : ""}
+            >
               Prom
             </NavDropdown.Item>
             <NavDropdown.Item
               as={Link}
               to="/flowergirl-firstcommunion"
               onClick={handleNavItemClick}
+              className={
+                location.pathname === "/flowergirl-firstcommunion"
+                  ? "active-link"
+                  : ""
+              }
             >
               Flower Girl & First Communion
             </NavDropdown.Item>
@@ -119,6 +160,9 @@ const CustomNavbar = () => {
               as={Link}
               to="/accessories"
               onClick={handleNavItemClick}
+              className={
+                location.pathname === "/accessories" ? "active-link" : ""
+              }
             >
               Accessories
             </NavDropdown.Item>
@@ -126,6 +170,9 @@ const CustomNavbar = () => {
               as={Link}
               to="/suits-tuxedos"
               onClick={handleNavItemClick}
+              className={
+                location.pathname === "/suits-tuxedos" ? "active-link" : ""
+              }
             >
               Suits & Tuxedos
             </NavDropdown.Item>
@@ -133,22 +180,38 @@ const CustomNavbar = () => {
               as={Link}
               to="/how-to-measure-yourself"
               onClick={handleNavItemClick}
+              className={
+                location.pathname === "/how-to-measure-yourself"
+                  ? "active-link"
+                  : ""
+              }
             >
               How to Measure Yourself
             </NavDropdown.Item>
           </NavDropdown>
           <NavDropdown
             title="Appointments"
-            className="dropdown"
             id="appointmentsDropdown"
             onMouseEnter={() => handleDropdownEnter("appointmentsDropdown")}
             onMouseLeave={() => handleDropdownLeave("appointmentsDropdown")}
             show={activeDropdown === "appointmentsDropdown"}
+            className={
+              location.pathname.includes("/schedule-appointment") ||
+              location.pathname.includes("/plan-appointment") ||
+              location.pathname.includes("/private-appointment")
+                ? "active-dropdown dropdown"
+                : "dropdown"
+            }
           >
             <NavDropdown.Item
               as={Link}
               to="/schedule-appointment"
               onClick={handleNavItemClick}
+              className={
+                location.pathname === "/schedule-appointment"
+                  ? "active-link"
+                  : ""
+              }
             >
               Schedule Appointment
             </NavDropdown.Item>
@@ -156,6 +219,9 @@ const CustomNavbar = () => {
               as={Link}
               to="/plan-appointment"
               onClick={handleNavItemClick}
+              className={
+                location.pathname === "/plan-appointment" ? "active-link" : ""
+              }
             >
               Plan Appointment
             </NavDropdown.Item>
@@ -163,13 +229,23 @@ const CustomNavbar = () => {
               as={Link}
               to="/private-appointment"
               onClick={handleNavItemClick}
+              className={
+                location.pathname === "/private-appointment"
+                  ? "active-link"
+                  : ""
+              }
             >
               Private Appointment
             </NavDropdown.Item>
           </NavDropdown>
           <NavDropdown
             title="Reviews"
-            className="dropdown"
+            className={
+              location.pathname.includes("/rave-reviews") ||
+              location.pathname.includes("/write-review")
+                ? "active-dropdown dropdown"
+                : "dropdown"
+            }
             id="reviewsDropdown"
             onMouseEnter={() => handleDropdownEnter("reviewsDropdown")}
             onMouseLeave={() => handleDropdownLeave("reviewsDropdown")}
@@ -179,6 +255,9 @@ const CustomNavbar = () => {
               as={Link}
               to="/rave-reviews"
               onClick={handleNavItemClick}
+              className={
+                location.pathname === "/rave-reviews" ? "active-link" : ""
+              }
             >
               Rave Reviews
             </NavDropdown.Item>
@@ -186,14 +265,27 @@ const CustomNavbar = () => {
               as={Link}
               to="/write-review"
               onClick={handleNavItemClick}
+              className={
+                location.pathname === "/write-review" ? "active-link" : ""
+              }
             >
               Write Review
             </NavDropdown.Item>
           </NavDropdown>
-          <Nav.Link as={Link} to="/events" onClick={handleNavItemClick}>
+          <Nav.Link
+            as={Link}
+            to="/events"
+            onClick={handleNavItemClick}
+            className={location.pathname === "/events" ? "active-link" : ""}
+          >
             Events
           </Nav.Link>
-          <Nav.Link as={Link} to="/contact" onClick={handleNavItemClick}>
+          <Nav.Link
+            as={Link}
+            to="/contact"
+            onClick={handleNavItemClick}
+            className={location.pathname === "/contact" ? "active-link" : ""}
+          >
             Contact Us
           </Nav.Link>
         </Nav>
