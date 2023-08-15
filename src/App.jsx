980b3496f1ad72b4d1ca46bca991e9e9311b404b
JordanReady <jready112@gmail.com>
2023-08-15 16:47:00 -0500

    fix: footer position

--- src/App.jsx
+++ src/App.jsx
@@ -61,50 +61,52 @@ const App = () => {
         <WelcomeHome />
       ) : (
         <div className="App">
-          <CustomNavbar />
-          <Routes>
-            <Route path="/" exact element={<Home />} />
-            <Route path="/our-story" exact element={<OurStory />} />
-            <Route path="/vendors" exact element={<Vendors />} />
-            <Route path="/bridal" exact element={<Bridal />} />
-            <Route
-              path="/bridesmaids-mothers"
-              exact
-              element={<Bridesmaids />}
-            />
-            <Route path="/prom" exact element={<Prom />} />
-            <Route
-              path="/flowergirl-firstcommunion"
-              exact
-              element={<FlowerGirl />}
-            />
-            <Route path="/accessories" exact element={<Accessories />} />
-            <Route path="/suits-tuxedos" exact element={<SuitsTuxedos />} />
-            <Route
-              path="/how-to-measure-yourself"
-              exact
-              element={<HowToMeasureYourself />}
-            />
-            <Route
-              path="/schedule-appointment"
-              exact
-              element={<ScheduleAppointment />}
-            />
-            <Route
-              path="/plan-appointment"
-              exact
-              element={<PlanAppointment />}
-            />
-            <Route
-              path="/private-appointment"
-              exact
-              element={<PrivateAppointment />}
-            />
-            <Route path="/rave-reviews" exact element={<RaveReviews />} />
-            <Route path="/write-review" exact element={<WriteReview />} />
-            <Route path="/events" exact element={<Events />} />
-            <Route path="/contact" exact element={<Contact />} />
-          </Routes>
+          <div className="page-content">
+            <CustomNavbar />
+            <Routes>
+              <Route path="/" exact element={<Home />} />
+              <Route path="/our-story" exact element={<OurStory />} />
+              <Route path="/vendors" exact element={<Vendors />} />
+              <Route path="/bridal" exact element={<Bridal />} />
+              <Route
+                path="/bridesmaids-mothers"
+                exact
+                element={<Bridesmaids />}
+              />
+              <Route path="/prom" exact element={<Prom />} />
+              <Route
+                path="/flowergirl-firstcommunion"
+                exact
+                element={<FlowerGirl />}
+              />
+              <Route path="/accessories" exact element={<Accessories />} />
+              <Route path="/suits-tuxedos" exact element={<SuitsTuxedos />} />
+              <Route
+                path="/how-to-measure-yourself"
+                exact
+                element={<HowToMeasureYourself />}
+              />
+              <Route
+                path="/schedule-appointment"
+                exact
+                element={<ScheduleAppointment />}
+              />
+              <Route
+                path="/plan-appointment"
+                exact
+                element={<PlanAppointment />}
+              />
+              <Route
+                path="/private-appointment"
+                exact
+                element={<PrivateAppointment />}
+              />
+              <Route path="/rave-reviews" exact element={<RaveReviews />} />
+              <Route path="/write-review" exact element={<WriteReview />} />
+              <Route path="/events" exact element={<Events />} />
+              <Route path="/contact" exact element={<Contact />} />
+            </Routes>
+          </div>
           <Footer />
         </div>
       )}
