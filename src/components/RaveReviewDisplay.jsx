166061d4ed0cccb2595a22c38cd20ae1e95ea559
JordanReady <jready112@gmail.com>
2023-08-15 22:38:58 -0500

    fix: rave reviews styles

--- src/components/RaveReviewDisplay.jsx
+++ src/components/RaveReviewDisplay.jsx
@@ -23,23 +23,24 @@ const RaveReviewDisplay = ({ review, style }) => {
 
   return (
     <div className="rave-review rave-review-animation" style={style}>
-      <div className="row rave-review-name-rating-row">
-        <div className="col-8">
-          <h2 className="rave-review-title">{displayName}</h2>
+      <div className="rave-review-info">
+        <div className="row rave-review-name-rating-row">
+          <div className="col-8">
+            <h2 className="rave-review-title">{displayName}</h2>
+          </div>
+          <div className="col-4 p-0">
+            <div className="rave-review-rating">{starIcons}</div>
+          </div>
         </div>
-        <div className="col-4 p-0">
-          <div className="rave-review-rating">{starIcons}</div>
+        <div className="row rave-review-role-date-row">
+          <div className="col-8">
+            <p className="rave-review-role">{role}</p>
+          </div>
+          <div className="col-4">
+            <p className="rave-review-date-published">{datePublished}</p>
+          </div>
         </div>
       </div>
-      <div className="row rave-review-role-date-row">
-        <div className="col-8">
-          <p className="rave-review-role">{role}</p>
-        </div>
-        <div className="col-4">
-          <p className="rave-review-date-published">{datePublished}</p>
-        </div>
-      </div>
-      <hr className="rave-review-hr" />
       <p className="rave-review-feedback">{feedback}</p>
     </div>
   );
